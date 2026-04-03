53b1c151e737e1aa271127a92718333d7f668931
Tembo Daniel <arthamptembo@gmail.com>
2026-04-03 10:14:22 -0400

    header, footer, pages

---
+++ src/lib/Header.svelte
@@ -0,0 +1,35 @@
+<header>
+    <div class="outer">
+        <div class="inner spread-apart">
+            <a href="/"><img src="/logo-dark.svg" alt="dark logo"></a>
+            <nav>
+                <ul class="spread-apart">
+                    <li>
+                        <a href="/">home</a>
+                    </li>
+                     <li>
+                        <a href="/about">about</a>
+                    </li>
+                    <li>
+                        <a href="/blog">blog</a>
+                    </li>
+                    <li>
+                        <a href="/contact">contact</a>
+                    </li>
+                    <li>
+                        <a href="/blog" class="button" bg-gradient-pink white>Get Started</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</header>
+
+<style>
+.inner{
+        padding-block: 1.5rem;
+    }
+    ul{
+        list-style: none;
+    }
+</style>
