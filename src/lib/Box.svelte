e0e7fea956e95624b6de760106d0b025710f742b
Tembo Daniel <arthamptembo@gmail.com>
2026-04-16 14:11:14 -0400

    v1 of home page

---
+++ src/lib/Box.svelte
@@ -0,0 +1,37 @@
+<script>
+let {
+    heading,
+    src,
+    description = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis, magni.",
+    
+} = $props()
+</script>
+
+<div class="flow bg-white">
+<img {src} alt="">
+<h3>heading</h3>
+<p>{description}</p>
+<a href="">Read More &raquo;</a>
+</div>
+
+<style>
+div {
+    position: relative;
+    padding: 2rem;
+    border-radius: 1rem;
+    font-size: 1.125rem;
+}
+
+img {
+    max-width: 3.5rem;
+    position: absolute;
+    top: -1.5rem;
+}
+
+a {
+display: inline-block;
+font-weight: bold;
+}
+
+
+</style>
