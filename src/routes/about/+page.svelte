223fc1c9b0e91128f56afabbe4d6e0425443f8d0
Tembo Daniel <arthamptembo@gmail.com>
2026-04-23 16:23:27 -0400

    v1 of about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -3,3 +3,48 @@
 
 </script>
 <Title name="about" />
+
+<section>
+	<div class="outer">
+		<div class="inner flow">
+			<h2>Subheading One</h2>
+		    <div class="columns flow">
+			  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, cumque qui aperiam aliquid sit temporibus, iste enim dolore quaerat ea repudiandae sapiente rerum quae exercitationem quia! Similique ratione ducimus provident mollitia dignissimos.</p>
+			  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat delectus doloribus qui doloremque nostrum? Distinctio unde hic illo. Tempore animi delectus perferendis voluptates itaque libero ratione? Cupiditate, eaque! Architecto maiores molestias repudiandae totam deleniti incidunt nulla vel. Velit consectetur quasi distinctio repellat.</p>
+			  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic laudantium libero, dolorem maxime sint accusantium sunt veniam assumenda ipsa quod recusandae sit atque quibusdam consequatur adipisci, nobis quia, mollitia earum error soluta ea! Ex id, ea perferendis aut impedit itaque facilis sapiente laudantium aperiam praesentium at recusandae excepturi asperiores ullam ut illum!</p>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section>
+	<div class="outer bg-blue white">
+		<div class="inner flow">
+			<h2>Subheading Two</h2>
+		    <div class="columns flow">
+			  <p class="white">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, cumque qui aperiam aliquid sit temporibus, iste enim dolore quaerat ea repudiandae sapiente rerum quae exercitationem quia! Similique ratione ducimus provident mollitia dignissimos.</p>
+			  <p class="white">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat delectus doloribus qui doloremque nostrum? Distinctio unde hic illo. Tempore animi delectus perferendis voluptates itaque libero ratione? Cupiditate, eaque! Architecto maiores molestias repudiandae totam deleniti incidunt nulla vel. Velit consectetur quasi distinctio repellat.</p>
+			  <p class="white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic laudantium libero, dolorem maxime sint accusantium sunt veniam assumenda ipsa quod recusandae sit atque quibusdam consequatur adipisci, nobis quia, mollitia earum error soluta ea! Ex id, ea perferendis aut impedit itaque facilis sapiente laudantium aperiam praesentium at recusandae excepturi asperiores ullam ut illum!</p>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section>
+	<div class="outer">
+		<div class="inner flow wide-text">
+			<h2>Subheading Three</h2>
+			<img class="float-right" src="https://picsum.photos/500/400" alt="">
+			  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae, cumque qui aperiam aliquid sit temporibus, iste enim dolore quaerat ea repudiandae sapiente rerum quae exercitationem quia! Similique ratione ducimus provident mollitia dignissimos.</p>
+			  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat delectus doloribus qui doloremque nostrum? Distinctio unde hic illo. Tempore animi delectus perferendis voluptates itaque libero ratione? Cupiditate, eaque! Architecto maiores molestias repudiandae totam deleniti incidunt nulla vel. Velit consectetur quasi distinctio repellat.</p>
+			  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic laudantium libero, dolorem maxime sint accusantium sunt veniam assumenda ipsa quod recusandae sit atque quibusdam consequatur adipisci, nobis quia, mollitia earum error soluta ea! Ex id, ea perferendis aut impedit itaque facilis sapiente laudantium aperiam praesentium at recusandae excepturi asperiores ullam ut illum!ipsa quod recusandae sit atque quibusdam consequatur adipisci, nobis quia, mollitia earum error soluta ea! Ex id, ea perferendis aut impedit itaque facilis sapiente laudantium aperiam praesentium at recusandae excepturi asperiores ullam ut illum!</p>
+		</div>
+	</div>
+</section>
+
+<style>
+.wide-text p {
+	max-width: unset;
+}
+</style>
+
