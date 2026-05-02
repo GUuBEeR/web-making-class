efd44402c3fa0b41284200e3504daa5793864fc5
Tembo Daniel <arthamptembo@gmail.com>
2026-05-02 17:22:28 -0400

    using mfs art repo for blog posts

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,5 +1,22 @@
 <script>
 	import Title from "$lib/Title.svelte";
 
+export let data
 </script>
 <Title name="blog" />
+
+<section>
+	<div class="outer">
+		<div class="inner auto-fit">
+
+			{#each data.posts.posts as post}
+				<div class="flow">
+				<img src="{post.featured_image}" alt="{post.title}">
+				<h2><a href="/blog/{post.slug}">{@html post.title}</a></h2>
+				<p>{@html post.excerpt}</p>
+			</div>
+			{/each}
+			
+		</div>
+	</div>
+</section>
